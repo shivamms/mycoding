a97526c8c40fc85ae18aabcfdf9232f17b9cf2aa
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2020-11-27 06:06:48 +0000

    added maxSubArray

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 #import haspair as hp
 #import arrays.implementArray as array
 #import arrays.reverseString as rs
-import arrays.mergeSortedArrays as msa
+#import arrays.mergeSortedArrays as msa
+import arrays.maxSubArray as msub
 
 #print(cl.comparelists([1,'a'], ['f',None,5]))
 #print(hp.haspairsumhash2([5,10,7,4,2,8,0],8))
@@ -10,6 +11,13 @@ import arrays.mergeSortedArrays as msa
 #print(rs.reverseStringSimple('subramanian'))
 #print(rs.reverseStringBetter('subramanian'))
 #print(rs.reverseStringSmarter('subramanian'))
-print(msa.mergeSortedArrays([0,4,6,8,31],[3,7,8,33,40,44]))
+#print(msa.mergeSortedArrays([0,4,6,8,31],[3,7,8,33,40,44]))
 #print(msa.mergeSortedArraysSmarter([4,4,4,4,4],[4,4,4,4]))
 
+#print(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+#print(maxSubArray([2,4,-3,4,-1,2,1,-5,4]))
+#print(maxSubArray([-2,-4,-3,4,-1,-2,-1,-5,-4]))
+#print(maxSubArray([-2,4,-3,4,-1,-2,-1,-5,-4]))
+#print(maxSubArray([-2,-4,-3,-4,-1,-2,-1,-5,-4]))
+#print(maxSubArray([1]))
+#print(maxSubArray([-2,-1]))
