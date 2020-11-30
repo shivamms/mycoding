62e08d47a6d1b2ae598902235f724544e26b076e
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2020-11-30 17:18:46 +0000

    Updated greedy alogorithm

--- arrays/maxSubArray.py
+++ arrays/maxSubArray.py
@@ -1,50 +1,31 @@
 #Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
-#Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
-#assume the number are not sorted
-#1 <= nums.length <= 2 * 104
-#-231 <= nums[i] <= 231 - 1
-#[-2,1,-3,4,-1,2,1,-5,4]
-"""
-def maximumSubarraySimple(arr):
-  if len(arr) == 1:
-    return arr
-  else:
-    currentmaximum = 0
-    maximum = 0
-    currentstart = 0
-    currentend = 0
-    originalstart= 0
-    for i in range(len(arr)):
-      currentmaximum = currentmaximum + arr[i]
-      if maximum < currentmaximum:
-        maximum = currentmaximum
-        currentend = i
-        originalstart = currentstart
-      if currentmaximum < 0:
-        currentmaximum = 0
-        currentstart = i + 1
-    if currentstart > currentend:
-      currentstart = originalstart
-    if currentend <= 0:
-      currentend = len(arr) - 1
-    return arr[currentstart:currentend+1]
-"""
-def maxSubArray(self, nums):
+#return maximum sum of contiguous numbers.
+#Time: O(n) and Space: O(1)
+#start with first number.
+#find local maximum by adding to the next number.
+#keep updating the global maximum if local maximum is greater than global maximum.
+#during any iteration, if global maximum is less than the current number, then update gloabl maximum and local maximum with current number.
+#setting up the global and local maximum to current number make sure we disregard the subarray which reduces the sum overall.
+
+def maxSubArray(nums):
   if len(nums) == 1:
     return nums[0]
   else:
-    currentmaximum = 0
-    maximum = -100
+    localmaximum = 0
+    globalmaximum = -2^31
     for i in range(len(nums)):
-      currentmaximum = currentmaximum + nums[i]
-      if maximum < currentmaximum:
-        maximum = currentmaximum
-      if maximum < nums[i]:
-        maximum = nums[i]
-      if currentmaximum < nums[i]:
-        currentmaximum = nums[i]
-    return maximum
+      localmaximum = localmaximum + nums[i]
+      if globalmaximum < localmaximum:
+        globalmaximum = localmaximum
+      if globalmaximum < nums[i]:
+        globalmaximum = nums[i]
+      if localmaximum < nums[i]:
+        localmaximum = nums[i]
+    return globalmaximum
+
+
+
 
 
 
