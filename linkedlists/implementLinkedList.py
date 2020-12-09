2866b641b8f63470e2b22470cc0545f4cc4d336d
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2020-12-09 15:51:54 +0000

    added doubly linked list class

--- linkedlists/implementLinkedList.py
+++ linkedlists/implementLinkedList.py
@@ -8,38 +8,208 @@ class linkedlist:
     self.head = node
     self.tail = node
     self.tail.next = None
+    self.length = 1
 
   def prepend(self, node):
-    node.next = self.head
+    oldheadnode = self.head
     self.head = node
+    self.head.next = oldheadnode
+    self.length += 1
   
   def append(self, node):
     self.tail.next = node
     self.tail = node
     self.tail.next = None
+    self.length += 1
 
   def insertafter(self, existingnode, newnode):
+    if self.tail.value == newnode.value:
+      self.tail = newnode
     nextnode = existingnode.next
     existingnode.next = newnode
     newnode.next = nextnode
+    self.length += 1
 
   def insertbefore(self, existingnode, newnode):
     node = self.head
+    previousnode = self.head
     while 1:
       if node.value == existingnode.value:
         previousnode.next = newnode
         newnode.next = existingnode
+        self.length += 1
         break
       previousnode = node
       node = node.next
+    if self.head.value == newnode.value:
+      self.head = newnode
+
+  def insertByIndex(self, index, newnode):
+    node = self.head
+    if index >= self.length:
+      self.tail.next = newnode
+      self.tail = newnode
+      self.tail.next = None
+    else:
+      for i in range(index):
+          node = node.next
+      newnode.next = node.next
+      node.next = newnode
+    self.length += 1
+
+  def removeByNode(self,delnode):
+    if self.head.value == delnode.value:
+      self.head = self.head.next
+      self.length -= 1
+      return
+    node = self.head
+    previousnode = self.head
+    for i in range(self.length):
+      if node.value == self.tail.value:
+        previousnode.next = None
+        self.tail = previousnode
+        self.length -= 1
+        return
+      elif node.value == delnode.value:
+        previousnode.next = node.next
+        self.length -= 1
+        return
+      previousnode = node
+      node = node.next
+  
+  def removeByIndex(self,index):
+    if index >= self.length:
+      return "Index out of range"
+    if index == 1:
+      self.head = self.head.next
+      self.length -= 1
+      return
+    node = self.head
+    previousnode = self.head
+    for i in range(index):
+      previousnode = node
+      node = node.next
+    previousnode.next = node.next
+    self.length -= 1
+    return
 
   def display(self):
     node = self.head
-    while 1:
-      print(node.value)
-      if node.next is None:
-        break
+    llistStr = ""
+    for i in range(self.length):
+      if i == 0:
+        llistStr = str(node.value)
+      else: 
+        llistStr = llistStr + "-->" + str(node.value)
       node = node.next
+    print(llistStr)
+
+
+class dlinkedlist:
+  def __init__ (self,node):
+    self.head = node
+    self.tail = node
+    self.head.prev = None
+    self.head.next = self.tail
+    self.tail.next = None
+    self.tail.prev = self.head
+    self.length = 1
+
+  def prepend(self, node):
+    self.head.prev = node
+    node.next = self.head
+    self.head = node
+    self.length += 1
+  
+  def append(self, node):
+    node.prev = self.tail
+    self.tail.next = node
+    self.tail = node
+    self.tail.next = None
+    self.length += 1
+
+  def insertafter(self, existingnode, newnode):
+    nextnode = existingnode.next
+    existingnode.next = newnode
+    newnode.prev = existingnode
+    nextnode.prev = newnode
+    newnode.next = nextnode
+    self.length += 1
+    if self.tail.value == newnode.value:
+      self.tail = newnode
+
+  def insertbefore(self, existingnode, newnode):
+    previousnode = existingnode.prev
+    previousnode.next = newnode
+    newnode.prev = previousnode
+    newnode.next = existingnode
+    self.length += 1
+    if self.head.value == newnode.value:
+      self.head = newnode
+
+  def insertByIndex(self, index, newnode):
+    node = self.head
+    if index >= self.length:
+      self.tail.next = newnode
+      newnode.prev = self.tail
+      self.tail = newnode
+      self.tail.next = None
+    else:
+      for i in range(index-1):
+          node = node.next
+      nextnode = node
+      newnode.next = node.next
+      newnode.prev = node
+      node.next = newnode
+      nextnode.prev = newnode
+    self.length += 1
+
+  def removeByNode(self,delnode):
+    if self.head.value == delnode.value:
+      self.head = self.head.next
+      self.length -= 1
+      return
+    node = self.head
+    previousnode = self.head
+    for i in range(self.length):
+      if node.value == self.tail.value:
+        previousnode.next = None
+        self.tail = previousnode
+        self.length -= 1
+        return
+      elif node.value == delnode.value:
+        previousnode.next = node.next
+        self.length -= 1
+        return
+      previousnode = node
+      node = node.next
+  
+  def removeByIndex(self,index):
+    if index >= self.length:
+      return "Index out of range"
+    if index == 1:
+      self.head = self.head.next
+      self.length -= 1
+      return
+    node = self.head
+    previousnode = self.head
+    for i in range(index):
+      previousnode = node
+      node = node.next
+    previousnode.next = node.next
+    self.length -= 1
+    return
+
+  def display(self):
+    node = self.head
+    llistStr = ""
+    for i in range(self.length):
+      if i == 0:
+        llistStr = str(node.value)
+      else: 
+        llistStr = llistStr + "<-->" + str(node.value)
+      node = node.next
+    print(llistStr)
 
 
 
--- main.py
+++ main.py
@@ -67,7 +67,8 @@ import linkedlists.implementLinkedList as ill
 #print(htfr.firstRecurring([1,2,2,2,2,2,2,3,3,2,3]))
 #print(htfr.firstRecurring([1,2,3,4,5,6,7,8,9,10,11]))
 #print(htfr.firstRecurring([]))
-
+"""
+# singly linked list test
 mynode = ill.node(14)
 nextnode = ill.node(18)
 mynode.next = nextnode
@@ -109,6 +110,77 @@ mylist.display()
 
 mylist.insertbefore(node4,node33)
 mylist.display()
+
+node8 = ill.node("ottagam")
+mylist.insertByIndex(7,node8)
+mylist.display()
+
+node9 = ill.node("avvaiyar")
+mylist.insertByIndex(8,node9)
+mylist.display()
+
+node10 = ill.node("oonai")
+mylist.insertByIndex(8,node10)
+mylist.display()
+print(mylist.length)
+"""
+
+# doubly linked list test
+mynode = ill.node(14)
+nextnode = ill.node(18)
+mynode.next = nextnode
+thirdnode = ill.node(20)
+nextnode.next = thirdnode
+
+node = mynode
+while 1:
+  print(node.value)
+  if node.next is None:
+    break
+  node = node.next
+
+node0 = ill.node("a")
+node1 = ill.node("amma")
+node2 = ill.node("aadu")
+node3 = ill.node("elai")
+mylist = ill.dlinkedlist(node1)
+mylist.append(node2)
+mylist.append(node3)
+mylist.prepend(node0)
+
+mylist.display()
+
+node0.value = "amma"
+node1.value = "aadu"
+node2.value = "elai"
+node3.value = "eli"
+node4 = ill.node("Iaindu")
+mylist.append(node4)
+
+mylist.display()
+
+node22 = ill.node("eee")
+node33 = ill.node("aeni")
+
+mylist.insertafter(node2,node22)
+mylist.display()
+
+mylist.insertbefore(node4,node33)
+mylist.display()
+
+node8 = ill.node("ottagam")
+mylist.insertByIndex(7,node8)
+mylist.display()
+
+node9 = ill.node("avvaiyar")
+mylist.insertByIndex(8,node9)
+mylist.display()
+
+node10 = ill.node("oonai")
+mylist.insertByIndex(8,node10)
+mylist.display()
+print(mylist.length)
+
 
 
 
