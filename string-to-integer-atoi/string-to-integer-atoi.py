1a026f50f13c69cfa7547afd18120c57d21666a4
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2021-02-12 22:48:46 +0000

    Time: 24 ms (98.48%), Space: 14.4 MB (28.95%) - LeetHub

--- string-to-integer-atoi/string-to-integer-atoi.py
+++ string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,27 +1,23 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        if len(s) == 0:
-            return 0
         s = s.lstrip(" ")
         if len(s) > 0:
             sign = ""
-            if s[0] == "-" or s[0] == "+":
+            if s[0] in ["-","+"]:
                 sign = s[0]
                 s = s[1:]
             for i, c in enumerate(s):
-                if ord(c) < 48 or ord(c) > 57:
+                val = ord(c)
+                if val < 48 or val > 57:
                     s = s[:i]
                     break
-            if len(s) == 0:
-                return 0
-            else:
+            if len(s) > 0:
                 n = int(sign + s)
                 if n < -2 ** 31:
                     return -2 ** 31
                 elif n > 2 ** 31 - 1:
                     return 2 ** 31 - 1
-            return n
-        else:
-            return 0
+                return n
+        return 0
             
         
