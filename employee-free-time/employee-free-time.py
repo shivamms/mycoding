a5e4ac506c788753b34878c98fc85f10dc28403c
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2021-02-16 00:41:09 -0500

    Time: 100 ms (35.91%), Space: 16.1 MB (45.48%) - LeetHub

---
+++ employee-free-time/employee-free-time.py
@@ -0,0 +1,30 @@
+"""
+# Definition for an Interval.
+class Interval:
+    def __init__(self, start: int = None, end: int = None):
+        self.start = start
+        self.end = end
+"""
+
+class Solution(object):
+    def employeeFreeTime(self, avails):
+        OPEN, CLOSE = 0, 1
+
+        events = []
+        for emp in avails:
+            for iv in emp:
+                events.append((iv.start, OPEN))
+                events.append((iv.end, CLOSE))
+        events.sort()
+        print(events)
+        ans = []
+        prev = None
+        bal = 0
+        for t, cmd in events:
+            if bal == 0 and prev is not None:
+                ans.append(Interval(prev, t))
+
+            bal += 1 if cmd is OPEN else -1
+            prev = t
+
+        return ans
