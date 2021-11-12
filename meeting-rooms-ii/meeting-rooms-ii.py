9514a019347256e528b92aceeaca178579db8dec
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2021-11-12 07:26:57 -0500

    Time: 72 ms (91.67%), Space: 17.4 MB (65.66%) - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.py
+++ meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,18 +1,25 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        confRooms = 1
-        intervals.sort(key=lambda x: x[0])
-        end = intervals[0][1]
-        roomsLatestEnd = []
-        heapq.heappush(roomsLatestEnd,intervals[0][1])
-        for i in range(1,len(intervals)):
-            if end > intervals[i][0]:
+        # confRooms = 1
+        # intervals.sort(key=lambda x: x[0])
+        # roomsLatestEnd = []
+        # heapq.heappush(roomsLatestEnd,intervals[0][1])
+        # for i in range(1,len(intervals)):
+        #     if roomsLatestEnd[0] > intervals[i][0]:
+        #         confRooms += 1
+        #     else:
+        #         heapq.heappop(roomsLatestEnd)
+        #     heapq.heappush(roomsLatestEnd,intervals[i][1])
+        # return confRooms
+        starts = sorted([i[0] for i in intervals])
+        ends = sorted([i[1] for i in intervals])
+        start = 0
+        end = 0
+        confRooms = 0
+        while start < len(starts):
+            if starts[start] < ends[end]:
                 confRooms += 1
-                heapq.heappush(roomsLatestEnd,intervals[i][1])
             else:
-                heapq.heappop(roomsLatestEnd)
-                heapq.heappush(roomsLatestEnd,intervals[i][1])
-            end = min(roomsLatestEnd)
+                end += 1
+            start += 1
         return confRooms
-        
-        
