66af326d93e03ce7edfee6b4a76b92bb07181823
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-01-02 12:50:27 -0500

    Time: 544 ms (5.00%), Space: 14.8 MB (33.42%) - LeetHub

--- number-of-provinces/number-of-provinces.py
+++ number-of-provinces/number-of-provinces.py
@@ -1,28 +1,41 @@
-from collections import defaultdict
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        def dfs(isConnected, visited, i):
-            for j in range(len(isConnected)):
-                if isConnected[i][j] == 1 and visited[j] == 0: 
-                    visited[j] = 1 
-                    dfs(isConnected, visited, j) 
+        class UnionFind:
+            def __init__(self, size):
+                self.root = [i for i in range(size)]
+                self.rank = [1] * size
+                self.count = size
 
-        length = len(isConnected)
-        visited = [0] * length
-        count = 0
-        for i in range(length):
-            if visited[i] == 0: 
-                dfs(isConnected, visited, i) 
-                count += 1
-        return count
-    
-#Notes:
-# 1. Make use of the given matrix as adjacent matrix.
-# 2. The whole input data structure is undirected graph.
-# 3. Index of each row in the matrix represents one node of the graph.
-# 4. Each 1 in the row represents the connection between the node in the row index and the node in the column index.
-# 5. Since the data structure is undirected graph, treat each node as root node and do a DFS from each node (N * DFS where N is number of nodes).
-# 6. Maintain a list of visited nodes, while doing DFS, make an entry in visited nodes list whenever we visit a node.
-# 7. Understand that for a DFS from any first node from a group of connected nodes, we get one province. Visits (DFS) from other nodes in the same province are ignored. Since we visit every node in a province (connected nodes). we complete visiting all the nodes in the province. So if we found a node already in the visited list, it means, it is part of some province and we do not need to count it.
+            def find(self, x):
+                if self.root[x] == x:
+                    return x
+                self.root[x] = self.find(self.root[x])
+                return self.root[x]
 
+            def union(self, x, y):
+                rootx = self.find(x)
+                rooty = self.find(y)
+                if rootx != rooty:
+                    if self.rank[rootx] > self.rank[rooty]:
+                        self.root[rooty] = rootx
+                    elif self.rank[rooty] > self.rank[rootx]:
+                        self.root[rootx] = rooty 
+                    else:
+                        self.root[rooty] = rootx
+                        self.rank[rootx] += 1
+                    self.count -= 1
+                print(self.root)
+
+            def getCount(self):
+                return self.count
+
+        rows = len(isConnected)
+        cols = len(isConnected[0])
+        uf = UnionFind(rows)
+        for i in range(len(isConnected)):
+            for j in range(len(isConnected[0])):
+                if isConnected[i][j] == 1:
+                    uf.union(i,j)
+
+        return uf.getCount()
         
