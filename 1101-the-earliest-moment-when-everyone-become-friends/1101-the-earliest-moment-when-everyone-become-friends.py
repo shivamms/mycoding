d0402d9bf95b42400c40fbd1e91324c15448685f
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-01-05 16:28:41 -0500

    Time: 208 ms (6.56%), Space: 14.7 MB (47.62%) - LeetHub

--- 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.py
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.py
@@ -3,31 +3,32 @@ class Solution:
         class UnionFind:
             def __init__(self,n):
                 self.root = [i for i in range(n)]
+                self.count = n
+                self.timestamp = -1
 
             def find(self, x):
                 while self.root[x] != x:
                     x = self.root[x]
                 return x
 
-            def union(self, x,y):
+            def union(self, timestamp, x,y):
                 rootx = self.find(x)
                 rooty = self.find(y)
                 if rootx != rooty:
                     self.root[rooty] = rootx
+                    self.count -= 1
+                    self.timestamp = timestamp
+                if self.count == 1:
+                    return True
+                return False
 
-            def allAcq(self):
-                rootnode = self.find(0)
-                for i in range(1,n):
-                    if rootnode != self.find(i):
-                        return False
-                return True
+            def getTimestamp(self):
+                return self.timestamp
 
-        logs.sort()
         uf = UnionFind(n)
+        logs.sort()
         for i, l in enumerate(logs):
-            uf.union(l[1],l[2])
-            if i >= n-2:
-                if uf.allAcq():
-                    return l[0]
+            if uf.union(l[0],l[1],l[2]):
+                return uf.getTimestamp()
         return -1
         
