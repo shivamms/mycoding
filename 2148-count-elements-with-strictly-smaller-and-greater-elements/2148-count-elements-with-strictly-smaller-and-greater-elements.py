e8ef4c1874fef0f5745e35c915af1b4b72cc7381
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-01-27 20:53:11 -0500

    Time: 37 ms (92.05%), Space: 13.9 MB (98.81%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.py
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.py
@@ -1,32 +1,10 @@
-from collections import defaultdict
+from collections import Counter
 class Solution:
     def countElements(self, nums: List[int]) -> int:
-        result = 0
-        n = len(nums)
-        sindex, bindex = 0, 0
-        smaller, bigger = [0]*n, [0]*n
-        eligible = set()
-        for i in range(n):
-            if nums[sindex] < nums[i] < nums[bindex]:
-                smaller[i] += 1
-                bigger[i] += 1
-            elif nums[sindex] < nums[i] > nums[bindex]:
-                bigger[bindex] += 1
-                bindex = i
-                smaller[i] += 1
-            elif nums[sindex] > nums[i] < nums[bindex]:
-                smaller[sindex] += 1
-                sindex = i
-                bigger[i] += 1
-            elif nums[sindex] < nums[i] == nums[bindex]:
-                smaller[i] += 1
-            elif nums[sindex] == nums[i] < nums[bindex]:
-                bigger[i] += 1
-        for i in range(n):
-            if smaller[i] > 0 and bigger[i] > 0 or nums[i] in eligible:
-                eligible.add(nums[i])
-                result += 1
-        return result
+        count = Counter(nums)
+        return 0 if len(count) < 3 else len(nums) - count[min(count)] - count[max(count)]
+        
+        
         
             
                 
