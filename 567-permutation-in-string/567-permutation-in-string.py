789c195743f31dc77b3221f1b9d7ed2aac70f721
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-02-02 21:50:30 -0500

    Time: 4981 ms (13.26%), Space: 13.9 MB (99.72%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    from collections import Counter, defaultdict
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        set1 = Counter(s1)
+        l = len(s1)
+        for i in range(len(s2)):
+            # print(set(s2[i:i+l]))
+            # print(set1)
+            if set1 == Counter(s2[i:i+l]):
+                return True
+        return False
