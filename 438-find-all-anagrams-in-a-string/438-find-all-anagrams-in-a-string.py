ec2ac0b524ca6fa7517a1cdee734c554244717e4
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-02-03 06:59:36 -0500

    Time: 167 ms (54.33%), Space: 15.3 MB (41.86%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -1,12 +1,5 @@
 class Solution:
-    def findAnagrams(self, s: str, p: str) -> List[int]:
-        
-        def compareStrings(set1, set2):
-            for i in range(26):
-                if set1[i] != set2[i]:
-                    return False
-            return True
-        
+    def findAnagrams(self, s: str, p: str) -> List[int]:        
         l = len(p)
         m = len(s)
         if l > m:
@@ -17,11 +10,11 @@ class Solution:
         for i in range(l): set1[ord(p[i])-97] += 1
         for i in range(l): set2[ord(s[i])-97] += 1
         for i in range(l, m):
-            if compareStrings(set1, set2):
+            if set1 == set2:
                 result.append(i-l)
             set2[ord(s[i-l])-97] -= 1
             set2[ord(s[i])-97] += 1
-        if compareStrings(set1, set2):
+        if set1 == set2:
                 result.append(m-l)
         return result
             
