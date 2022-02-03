9b0bc1a835693162bde5c10853e135213fd65287
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-02-02 20:52:11 -0500

    Time: 36 ms (71.40%), Space: 13.9 MB (99.63%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.py
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.py
@@ -1,12 +1,5 @@
 class Solution:
-    def removeOccurrences(self, s: str, part: str) -> str:
-        ws = len(part)
-        n = len(s)
-        if n < ws:
-            return s
-        if s == part:
-            return ""
-        
+    def removeOccurrences(self, s: str, part: str) -> str:        
         while part in s:
             string = s.replace(part, '',1)
             s = string
