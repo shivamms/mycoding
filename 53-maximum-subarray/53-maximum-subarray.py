835cc1fcae466b7eeed9f9b08c36bbeb4754622d
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-02-09 10:58:50 -0500

    Time: 3999 ms (5.01%), Space: 27.8 MB (94.54%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,9 +1,35 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        maxSum = curSum = nums[0]
-        for num in nums[1:]:
-            curSum = max(num, curSum + num)
-            maxSum = max(curSum, maxSum)
-        return maxSum
+        # O(N) solution with DP
+        # maxSum = curSum = nums[0]
+        # for num in nums[1:]:
+        #     curSum = max(num, curSum + num)
+        #     maxSum = max(curSum, maxSum)
+        # return maxSum
+        
+        # divide and conquer
+        def dcMaxSubarray(nums, left, right):
+            if left > right:
+                return -math.inf
+            mid = (left + right) // 2
+            currSum = maxLeftSum = maxRightSum = 0
+            
+            for i in range(mid-1, left-1, -1):
+                currSum += nums[i]
+                maxLeftSum = max(maxLeftSum, currSum)
+            
+            currSum = 0
+            for i in range(mid+1, right+1):
+                currSum += nums[i]
+                maxRightSum = max(maxRightSum, currSum)
+            
+            maxSum = maxLeftSum + nums[mid] + maxRightSum
+            
+            leftSum = dcMaxSubarray(nums, left, mid-1)
+            rightSum = dcMaxSubarray(nums, mid+1, right)
+            
+            return max(maxSum, leftSum, rightSum)
+            
+        return dcMaxSubarray(nums, 0, len(nums) - 1)
                 
             
