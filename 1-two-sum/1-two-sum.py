0eaf12eb96097a9b8f233d33be16c73a8fc0abf4
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-02-09 15:54:24 -0500

    Time: 110 ms (47.21%), Space: 15.2 MB (49.69%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        comps = defaultdict(int)
+        for i, num in enumerate(nums):
+            if num in comps:
+                return i, comps[num]
+            comps[target-num] = i
+    
+            
+        
+        
