13e971307c441434f1c9e98af73b91b535adc699
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-02-11 08:00:30 -0500

    Time: 52 ms (19.81%), Space: 13.9 MB (79.77%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,8 +1,16 @@
 class Solution:
     fibval = {0:0, 1:1, 2:1}
     def tribonacci(self, n: int) -> int:
+        # recursion and memoization
+        # if n in self.fibval:
+        #     return self.fibval[n]
+        # self.fibval[n] = self.tribonacci(n-3) + self.tribonacci(n-2) + self.tribonacci(n-1)
+        # return self.fibval[n]
+    
+        # loop and memoization
         if n in self.fibval:
             return self.fibval[n]
-        self.fibval[n] = self.tribonacci(n-3) + self.tribonacci(n-2) + self.tribonacci(n-1)
+        for i in range(3,n+1):
+            self.fibval[i] = self.fibval[i-3] + self.fibval[i-2] + self.fibval[i-1]
         return self.fibval[n]
         
