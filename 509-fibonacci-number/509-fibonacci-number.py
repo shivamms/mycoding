7c9e5be554ab0e0b39d38208b377a77cb128f4ba
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-02-11 11:52:47 -0500

    Time: 56 ms (36.15%), Space: 13.9 MB (90.97%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -8,8 +8,13 @@ class Solution:
         # return self.fibval[n]
         
         # loop
-        if n <= 1:
-            return n
-        for i in range(2,n+1):
-            self.fibval[i] = self.fibval[i-1]+self.fibval[i-2]
-        return self.fibval[n]
+        # if n <= 1:
+        #     return n
+        # for i in range(2,n+1):
+        #     self.fibval[i] = self.fibval[i-1]+self.fibval[i-2]
+        # return self.fibval[n]
+    
+        # math - Golden Ratio
+        gr = (1+math.sqrt(5))/2
+        return int(round((gr ** n)/(5 ** 0.5)))
+        
