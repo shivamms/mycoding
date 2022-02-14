0173151ac5f4174e85ce1d3fa9c4f735f2192626
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-02-14 06:11:42 -0500

    Time: 60 ms (78.22%), Space: 14.3 MB (71.84%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -0,0 +1,20 @@
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        count = Counter(nums)
+        print(count)
+        score = leftScore = leftLeftScore = 0
+        prev = -1
+        for num in sorted(count):
+            if num - 1 != prev:
+                score += num * count[num]
+            else:
+                score = max(leftScore, leftLeftScore + num * count[num])
+            prev = num
+            temp = leftScore
+            leftScore = score
+            leftLeftScore = temp
+        return score
+                
+            
+                
+        
