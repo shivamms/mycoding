d02073c49a47a034f9dea604bdc09cf4e97c79e6
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-02-18 08:28:03 -0500

    Time: 172 ms (79.37%), Space: 15.6 MB (35.26%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,24 +3,33 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        zero = deque()
-        pos = None
         n = len(nums)
         i = 0
+        lastNonZero = 0
         while i<n:
             if nums[i] != 0:
-                pos = i
-                if zero and zero[0] < pos:
-                    nums[zero[0]], nums[pos] = nums[pos], nums[zero[0]]
-                    zero.popleft()
-                    zero.append(i)
-                    pos = None
-            else:
-                zero.append(i)
-                if pos and pos > zero[0]:
-                    nums[zero[0]], nums[pos] = nums[pos], nums[zero[0]]
-                    zero.popleft()
-                    zero.append(i)
-                    pos = None 
+                nums[i], nums[lastNonZero] = nums[lastNonZero], nums[i]
+                lastNonZero += 1
             i += 1
+#         zero = deque()
+#         pos = None
+#         n = len(nums)
+#         i = 0
+#         while i<n:
+#             if nums[i] != 0:
+#                 pos = i
+#                 if zero and zero[0] < pos:
+#                     nums[zero[0]], nums[pos] = nums[pos], nums[zero[0]]
+#                     zero.popleft()
+#                     zero.append(i)
+#                     pos = None
+#             else:
+#                 zero.append(i)
+#                 if pos and pos > zero[0]:
+#                     nums[zero[0]], nums[pos] = nums[pos], nums[zero[0]]
+#                     zero.popleft()
+#                     zero.append(i)
+#                     pos = None 
+#             i += 1
+
                 
