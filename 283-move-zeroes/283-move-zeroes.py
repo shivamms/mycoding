fbcbc1dfc3172559e127351744be11697c535d1d
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-02-18 08:15:28 -0500

    Time: 189 ms (66.24%), Space: 15.6 MB (13.95%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.py
@@ -0,0 +1,26 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zero = deque()
+        pos = None
+        n = len(nums)
+        i = 0
+        while i<n:
+            if nums[i] != 0:
+                pos = i
+                if zero and zero[0] < pos:
+                    nums[zero[0]], nums[pos] = nums[pos], nums[zero[0]]
+                    zero.popleft()
+                    zero.append(i)
+                    pos = None
+            else:
+                zero.append(i)
+                if pos and pos > zero[0]:
+                    nums[zero[0]], nums[pos] = nums[pos], nums[zero[0]]
+                    zero.popleft()
+                    zero.append(i)
+                    pos = None 
+            i += 1
+                
