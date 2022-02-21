30f3ec2788cf794741348e87f2082cebf23d5b88
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-02-21 16:24:07 -0500

    Time: 211 ms (42.71%), Space: 16.5 MB (57.66%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,23 +1,22 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         def checkIsland(r, c, curArea):
-            visited.add((r,c))
+            grid[r][c] = 0
             curArea += 1
-            if (r+1, c) not in visited and r+1 <= n-1 and grid[r+1][c] == 1:
+            if r+1 <= n-1 and grid[r+1][c] == 1:
                 curArea = checkIsland(r+1, c, curArea)
-            if (r-1, c) not in visited and r-1 >= 0 and grid[r-1][c] == 1:
+            if r-1 >= 0 and grid[r-1][c] == 1:
                 curArea = checkIsland(r-1, c, curArea)
-            if (r, c+1) not in visited and c+1 <= m-1 and grid[r][c+1] == 1:
+            if c+1 <= m-1 and grid[r][c+1] == 1:
                 curArea = checkIsland(r, c+1, curArea)
-            if (r, c-1) not in visited and c-1 >= 0 and grid[r][c-1] == 1:
+            if c-1 >= 0 and grid[r][c-1] == 1:
                 curArea = checkIsland(r, c-1, curArea)
             return curArea
         
         n, m = len(grid), len(grid[0])
-        visited = set()
         area = 0
         for i in range(n):
             for j in range(m):
-                if (i,j) not in visited and grid[i][j] == 1:
+                if grid[i][j] == 1:
                     area = max(area, checkIsland(i, j, 0))
         return area
