bbfc13bda13e9d345472098f0eb8c6990f1eb871
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-02-24 14:00:16 -0500

    Time: 53 ms (53.92%), Space: 14.3 MB (95.99%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
@@ -6,15 +6,32 @@
 #         self.right = right
 class Solution:
     def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+        ## recursion
+        
+        # node = root
+        # self.treeSum = 0
+        # def rootToLeaf(node, number):
+        #     if node:
+        #         number = (number << 1) | node.val
+        #         if node.left is None and node.right is None:
+        #             self.treeSum += number
+        #         rootToLeaf(node.left, number)
+        #         rootToLeaf(node.right, number)
+        # rootToLeaf(node, 0)
+        # return self.treeSum
+        
+        ## iteration
         node = root
-        self.treeSum = 0
-        def rootToLeaf(node, number):
+        number = treeSum = 0
+        stack = [(node, number)]
+        while stack:
+            node, number = stack.pop()
             if node:
                 number = (number << 1) | node.val
                 if node.left is None and node.right is None:
-                    self.treeSum += number
-                rootToLeaf(node.left, number)
-                rootToLeaf(node.right, number)
-        rootToLeaf(node, 0)
-        return self.treeSum
+                    treeSum += number
+                else:
+                    stack.append((node.left, number))
+                    stack.append((node.right, number))
+        return treeSum
         
