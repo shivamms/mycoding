cca818fdd9e29dc836f5c0404aa08304cd036ae5
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-02-26 16:21:29 -0500

    Time: 864 ms (65.63%), Space: 28.6 MB (42.22%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,35 +1,9 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # O(N) solution with DP
-        # maxSum = curSum = nums[0]
-        # for num in nums[1:]:
-        #     curSum = max(num, curSum + num)
-        #     maxSum = max(curSum, maxSum)
-        # return maxSum
-        
-        # divide and conquer
-        def dcMaxSubarray(nums, left, right):
-            if left > right:
-                return -math.inf
-            mid = (left + right) // 2
-            currSum = maxLeftSum = maxRightSum = 0
-            
-            for i in range(mid-1, left-1, -1):
-                currSum += nums[i]
-                maxLeftSum = max(maxLeftSum, currSum)
-            
-            currSum = 0
-            for i in range(mid+1, right+1):
-                currSum += nums[i]
-                maxRightSum = max(maxRightSum, currSum)
-            
-            maxSum = maxLeftSum + nums[mid] + maxRightSum
-            
-            leftSum = dcMaxSubarray(nums, left, mid-1)
-            rightSum = dcMaxSubarray(nums, mid+1, right)
-            
-            return max(maxSum, leftSum, rightSum)
-            
-        return dcMaxSubarray(nums, 0, len(nums) - 1)
+        maxSum, localSum = nums[0], nums[0]
+        for i in range(1,len(nums)):
+            localSum = max(localSum+nums[i], nums[i])
+            maxSum = max(localSum, maxSum)
+        return maxSum
                 
-            
+        
