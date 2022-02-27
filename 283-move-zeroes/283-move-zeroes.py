363626875dacda967a27ddc3d08567eaa2479a23
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-02-27 15:03:24 -0500

    Time: 176 ms (82.29%), Space: 15.6 MB (15.59%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,33 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
+        zeroIndices = deque()
         i = 0
-        lastNonZero = 0
-        while i<n:
-            if nums[i] != 0:
-                nums[i], nums[lastNonZero] = nums[lastNonZero], nums[i]
-                lastNonZero += 1
+        while i < len(nums):
+            if nums[i] == 0:
+                zeroIndices.append(i)
+            elif zeroIndices:
+                nums[zeroIndices.popleft()], nums[i] = nums[i], 0     
+                zeroIndices.append(i)
             i += 1
-#         zero = deque()
-#         pos = None
-#         n = len(nums)
-#         i = 0
-#         while i<n:
-#             if nums[i] != 0:
-#                 pos = i
-#                 if zero and zero[0] < pos:
-#                     nums[zero[0]], nums[pos] = nums[pos], nums[zero[0]]
-#                     zero.popleft()
-#                     zero.append(i)
-#                     pos = None
-#             else:
-#                 zero.append(i)
-#                 if pos and pos > zero[0]:
-#                     nums[zero[0]], nums[pos] = nums[pos], nums[zero[0]]
-#                     zero.popleft()
-#                     zero.append(i)
-#                     pos = None 
-#             i += 1
-
-                
+            
+        
