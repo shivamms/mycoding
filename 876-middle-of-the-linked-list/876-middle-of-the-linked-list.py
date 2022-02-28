50653c29c95c3bb0dadd1a1eb7e8a09251416811
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-02-27 21:19:41 -0500

    Time: 33 ms (73.73%), Space: 13.8 MB (80.13%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -14,20 +14,26 @@ class Solution:
         # return arr[(len(arr)//2)]
     
         ## two pass without extra space
-        node = head
-        count = 0
-        arr = []
-        while node:
-            arr.append(node)
-            count += 1
-            node = node.next
-        mid = (count // 2) + 1
-        node = head
-        i = 1
-        while i <= mid:
-            if i == mid:
-                return node
-            i += 1
-            node = node.next
+        # node = head
+        # count = 0
+        # arr = []
+        # while node:
+        #     arr.append(node)
+        #     count += 1
+        #     node = node.next
+        # mid = (count // 2) + 1
+        # node = head
+        # i = 1
+        # while i <= mid:
+        #     if i == mid:
+        #         return node
+        #     i += 1
+        #     node = node.next
         
+        ## Fast and slow pointer
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
         
