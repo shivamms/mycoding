d7a2467f10514950e3f8992e64889bc04b9642c5
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-02-28 11:52:40 -0500

    Time: 91 ms (83.26%), Space: 14.8 MB (65.47%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.py
+++ 566-reshape-the-matrix/566-reshape-the-matrix.py
@@ -1,18 +1,31 @@
 class Solution:
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        ## straight forward method
+        # m, n = len(mat), len(mat[0])
+        # if r*c != m*n:
+        #     return mat
+        # reshaped = [[0 for j in range(c)] for i in range(r)]
+        # x = y = 0
+        # for i in range(m):
+        #     for j in range(n):
+        #         reshaped[x][y] = mat[i][j]
+        #         y += 1
+        #         if y > c-1:
+        #             y = 0
+        #             x += 1
+        # return reshaped
+        
+        ## division and modulus method
         m, n = len(mat), len(mat[0])
         if r*c != m*n:
             return mat
         reshaped = [[0 for j in range(c)] for i in range(r)]
-        x = y = 0
+        size = 0
         for i in range(m):
             for j in range(n):
-                reshaped[x][y] = mat[i][j]
-                y += 1
-                if y > c-1:
-                    y = 0
-                    x += 1
+                reshaped[size // c][size % c] = mat[i][j]
+                size += 1
         return reshaped
-                
+        
         
         
