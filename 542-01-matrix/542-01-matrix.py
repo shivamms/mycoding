b75030f0dac1ac98c5e42013658d1faceab671c2
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-03-04 06:05:34 -0500

    Time: 890 ms (54.58%), Space: 17.4 MB (32.12%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -1,36 +1,21 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        ## BFS
-#         m, n = len(mat), len(mat[0])
-        
-#         queue = deque()
-#         queue.append((0,0))
-#         visited = set()
-#         while queue:
-#             (row, col) = queue.popleft()
-#             visited.add((row, col))
-#             for (r, c) in ((row+1, col), (row-1,col), (row, col+1), (row, col-1)):
-#                 if 0 <= r < m and 0 <= c < n and (r, c) not in visited:
-#                     if mat[r][c] != 0:
-#                         mat[r][c] = mat[row][col] + 1
-#                     queue.append((r,c))
-#         return mat
-        ## DP
         m, n = len(mat), len(mat[0])
-        result = [[math.inf for j in range(n)] for i in range(m)]
+        distance = [[math.inf for c in range(n)] for r in range(m)]
         for r in range(m):
             for c in range(n):
                 if mat[r][c] == 0:
-                    result[r][c] = 0
+                    distance[r][c] = 0
                 else:
-                    if r > 0:
-                        result[r][c] = min(result[r][c], result[r-1][c]+1)
-                    if c > 0:
-                        result[r][c] = min(result[r][c], result[r][c-1]+1)
-        for r in range(m-1, -1, -1):
+                    for (row, col) in ((r-1,c), (r, c-1)):
+                        if 0 <= row < m and 0 <= col < n:
+                            distance[r][c] = min(distance[row][col]+1, distance[r][c])
+        for r in range(m-1, -1 ,-1):
             for c in range(n-1, -1, -1):
-                if r < m-1:
-                    result[r][c] = min(result[r][c], result[r+1][c]+1)
-                if c < n-1:
-                    result[r][c] = min(result[r][c], result[r][c+1]+1)
-        return result
+                if mat[r][c] == 0:
+                    distance[r][c] = 0
+                else:
+                    for (row, col) in ((r+1,c), (r, c+1)):
+                        if 0 <= row < m and 0 <= col < n:
+                            distance[r][c] = min(distance[row][col]+1, distance[r][c])
+        return distance
