63290b6836d262fa20152a22d2b0b35eeec122a4
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-03-07 01:09:58 -0500

    Time: 572 ms (73.14%), Space: 33.9 MB (89.20%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -0,0 +1,38 @@
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        class UnionFind:
+            def __init__(self, size):
+                self.root = [i for i in range(size)]
+                self.size = size
+                self.cables = 0
+                
+            def find(self, x):
+                if self.root[x] == x:
+                    return x
+                self.root[x] = self.find(self.root[x])
+                return self.root[x]
+            
+            def union(self, x,y):
+                rootx = self.find(x)
+                rooty = self.find(y)
+                if rootx != rooty:
+                    self.root[rooty] = rootx
+                else:
+                    self.cables += 1
+            
+        uf = UnionFind(n)
+        for conn in connections:
+            uf.union(conn[0],conn[1])
+        roots = set()
+        for i in range(n):
+            roots.add(uf.find(i))
+        if len(roots) == 1:
+            return 0
+        elif len(roots)-1 == uf.cables:
+            return uf.cables
+        elif len(roots)-1 > uf.cables:
+            return -1
+        elif len(roots)-1 < uf.cables:
+            return len(roots)-1
+        
+        
