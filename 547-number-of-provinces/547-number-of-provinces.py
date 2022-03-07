67440ecc9ffa5c9e74f5bf765d5ee486c9d98df4
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-03-06 23:53:56 -0500

    Time: 286 ms (50.62%), Space: 14.9 MB (25.54%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,34 +1,50 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        class UnionFind:
-            def __init__(self, size):
-                self.root = [i for i in range(size)]
+        ## using UnionFind
+#         class UnionFind:
+#             def __init__(self, size):
+#                 self.root = [i for i in range(size)]
                 
-            def find(self, x):
-                while self.root[x] != x:
-                    x = self.root[x]
-                return x
+#             def find(self, x):
+#                 while self.root[x] != x:
+#                     x = self.root[x]
+#                 return x
             
-            def union(self, x,y):
-                rootx = self.find(x)
-                rooty = self.find(y)
-                if rootx != rooty:
-                    self.root[rooty] = rootx
+#             def union(self, x,y):
+#                 rootx = self.find(x)
+#                 rooty = self.find(y)
+#                 if rootx != rooty:
+#                     self.root[rooty] = rootx
                     
-            def connected(self, x,y):
-                return find(x) == find(y)
+#             def connected(self, x,y):
+#                 return find(x) == find(y)
         
-        provinces = set()
-        n = len(isConnected)
-        uf = UnionFind(n)
-        for r in range(n):
-            for c in range(n):
-                if isConnected[r][c]:
-                    uf.union(r,c)
+#         provinces = set()
+#         n = len(isConnected)
+#         uf = UnionFind(n)
+#         for r in range(n):
+#             for c in range(n):
+#                 if isConnected[r][c]:
+#                     uf.union(r,c)
             
+#         for r in range(n):
+#             provinces.add(uf.find(r))
+#         return len(provinces)
+    
+        ## DFS
+        n = len(isConnected)
+        visited = set()
+        def dfs(r):
+            for c in range(n):
+                if c not in visited and isConnected[r][c] == 1:
+                    visited.add(c)
+                    dfs(c)
+
+        count = 0;
         for r in range(n):
-            provinces.add(uf.find(r))
-        return len(provinces)
-        
+            if r not in visited:
+                dfs(r)
+                count += 1
+        return count
                     
             
