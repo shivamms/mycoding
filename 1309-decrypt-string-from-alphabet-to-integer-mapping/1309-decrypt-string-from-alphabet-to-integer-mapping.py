0787313128895284db929f3ac65298d2e346c884
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-03-07 13:13:57 -0500

    Time: 30 ms (89.73%), Space: 13.9 MB (49.32%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -1,11 +1,24 @@
 class Solution:
     def freqAlphabets(self, s: str) -> str:
-        n = len(s)
-        queue = [s[0]]
-        for i in range(1,n):
+        ## using xtra space - 98%
+        # n = len(s)
+        # queue = [s[0]]
+        # for i in range(1,n):
+        #     if s[i] == '#':
+        #         l2, l1 = queue.pop(), queue.pop()
+        #         queue.append(l1+l2)
+        #     else:
+        #         queue.append(s[i])
+        # return ''.join([chr(96+int(x)) for x in queue])
+    
+        ## without xtra space
+        decrypted, i = '', len(s)-1
+        while i >= 0:
             if s[i] == '#':
-                l2, l1 = queue.pop(), queue.pop()
-                queue.append(l1+l2)
+                decrypted += chr(96+int(s[i-2:i]))
+                i -= 3
             else:
-                queue.append(s[i])
-        return ''.join([chr(96+int(x)) for x in queue])
+                decrypted += chr(96+int(s[i]))
+                i -= 1
+        return decrypted[::-1]
+        
