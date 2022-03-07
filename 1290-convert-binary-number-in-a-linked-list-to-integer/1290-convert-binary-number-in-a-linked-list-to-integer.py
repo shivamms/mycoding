eb61ad628637c0503aa98f856a02703039839fbf
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-03-07 15:14:35 -0500

    Time: 62 ms (10.71%), Space: 13.8 MB (73.78%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.py
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.py
@@ -15,8 +15,14 @@ class Solution:
         #     decimalVal += (2**n) * binary[i]
         #     n -= 1
         # return decimalVal
+        # num = head.val
+        # while head.next:
+        #     num = num * 2 + head.next.val
+        #     head = head.next
+        # return num
+        ## bit manipulation
         num = head.val
         while head.next:
-            num = num * 2 + head.next.val
+            num = (num << 1) | head.next.val
             head = head.next
         return num
