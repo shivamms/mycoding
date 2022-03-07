32e894146f0857d2d1a4fb695ef2d53e9b6fb982
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-03-07 15:59:53 -0500

    Time: 51 ms (31.10%), Space: 13.8 MB (74.34%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -5,35 +5,8 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        ## using extra space
-        # arr = []
-        # node = head
-        # while node:
-        #     arr.append(node)
-        #     node = node.next
-        # return arr[(len(arr)//2)]
-    
-        ## two pass without extra space
-        # node = head
-        # count = 0
-        # arr = []
-        # while node:
-        #     arr.append(node)
-        #     count += 1
-        #     node = node.next
-        # mid = (count // 2) + 1
-        # node = head
-        # i = 1
-        # while i <= mid:
-        #     if i == mid:
-        #         return node
-        #     i += 1
-        #     node = node.next
-        
-        ## Fast and slow pointer
         slow = fast = head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
         return slow
-        
