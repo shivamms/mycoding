9608bdc05e6974b8610501c98c34c7c369bda611
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-03-07 02:18:30 -0500

    Time: 176 ms (34.16%), Space: 14.2 MB (72.49%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -1,15 +1,18 @@
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
           ## Recursion
-        n = len(mat) 
-        def getSum(r,c, dsum, colDir):
-            if r <= n-1 and 0 <= c:
-                dsum = mat[r][c] + getSum(r+1, c+colDir, dsum, colDir)
-            return dsum
+#         n = len(mat) 
+#         def getSum(r,c, dsum, colDir):
+#             if r <= n-1 and 0 <= c:
+#                 dsum = mat[r][c] + getSum(r+1, c+colDir, dsum, colDir)
+#             return dsum
         
-        dsum1 = getSum(0,0,0, 1) 
-        dsum2 = getSum(0,n-1, 0, -1) 
-        return dsum1 + dsum2 if n % 2 == 0 else dsum1 + dsum2 - mat[n//2][n//2]
-        # n, dsum = len(mat), 0
-        # while r 
-        #         dsum += mat[r][c]
+#         dsum1 = getSum(0,0,0, 1) 
+#         dsum2 = getSum(0,n-1, 0, -1) 
+#         return dsum1 + dsum2 if n % 2 == 0 else dsum1 + dsum2 - mat[n//2][n//2]
+        n, dsum, r, c = len(mat), 0, 0, 0
+        while r <= n-1 and c >= 0:
+                dsum += mat[r][c] + mat[r][n-1-c]
+                c += 1
+                r += 1
+        return dsum if n % 2 == 0 else dsum - mat[n//2][n//2]
