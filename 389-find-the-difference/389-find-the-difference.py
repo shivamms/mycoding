f746fd46b556f337fca053a636f09b72b594084a
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-03-07 11:52:55 -0500

    Time: 24 ms (99.72%), Space: 14 MB (39.38%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,11 +1,18 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        s,t = Counter(s), Counter(t)
-        if len(s) > len(t):
-            for key, val in s.items():
-                if val != t[key] or key not in t:
-                    return key
-        else:
-            for key, val in t.items():
-                if val != s[key] or key not in s:
-                    return key
+        ## hashmap
+        # s,t = Counter(s), Counter(t)
+        # if len(s) > len(t):
+        #     for key, val in s.items():
+        #         if val != t[key] or key not in t:
+        #             return key
+        # else:
+        #     for key, val in t.items():
+        #         if val != s[key] or key not in s:
+        #             return key
+                
+        ## bit manipulation - XOR
+        ch = 0
+        for l in s: ch ^= ord(l)
+        for l in t: ch ^= ord(l)
+        return chr(ch)
