a6a071e1d9b037ef8a197deab32aee23658781fb
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-03-07 22:29:02 -0500

    Time: 68 ms (91.16%), Space: 14.1 MB (88.58%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -1,8 +1,9 @@
 class Solution:
     def sortByBits(self, arr: List[int]) -> List[int]:
-        bits = []
-        for num in arr:
-            bits.append((Counter(str(bin(num)))["1"], num))
-        bits.sort(key = lambda p: (p[0],p[1]))
-        return [p[1] for p in bits]
+        return sorted([num for num in arr], key=lambda x:(bin(x).count("1"),x))
+        # bits = []
+        # for num in arr:
+        #     bits.append((Counter(str(bin(num)))["1"], num))
+        # bits.sort(key = lambda p: (p[0],p[1]))
+        # return [p[1] for p in bits]
         
