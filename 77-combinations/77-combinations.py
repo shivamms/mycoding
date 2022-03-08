916902d7dc8b94e7645f784e10e0348f0ebf867b
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-03-08 13:27:22 -0500

    Time: 655 ms (31.37%), Space: 16.1 MB (17.69%) - LeetHub

---
+++ 77-combinations/77-combinations.py
@@ -0,0 +1,12 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        def backtrack(first, curr):
+            if len(curr) == k:
+                output.append(curr[:])
+            for i in range(first, n+1):
+                curr.append(i)
+                backtrack(i + 1, curr)
+                curr.pop()
+        output = []
+        backtrack(1,[])
+        return output
