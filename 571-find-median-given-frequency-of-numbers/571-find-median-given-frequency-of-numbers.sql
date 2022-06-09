8b30d827e2b037a134aa3c080df5f51275807388
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-06-09 09:12:13 -0400

    Time: 401 ms (23.64%), Space: 0B (100.00%) - LeetHub

--- 571-find-median-given-frequency-of-numbers/571-find-median-given-frequency-of-numbers.sql
+++ 571-find-median-given-frequency-of-numbers/571-find-median-given-frequency-of-numbers.sql
@@ -4,9 +4,9 @@ WITH RECURSIVE expanded AS
 UNION ALL
 SELECT num, frequency, n + 1 FROM expanded WHERE n < frequency)
 , expanded_with_rownum AS (SELECT num, frequency, ROW_NUMBER() OVER(ORDER BY num, n) AS rownum FROM expanded)
-, expanded_max_rownum AS (SELECT MAX(rownum) AS max_rownum FROM expanded_with_rownum)
-, expanded_with_mid AS (SELECT max_rownum,
-CASE WHEN MOD(max_rownum, 2) = 0 THEN (max_rownum/2) ELSE CEILING(max_rownum/2) END AS mid1,
-CASE WHEN MOD(max_rownum, 2) = 0 THEN (max_rownum/2)+1 ELSE CEILING(max_rownum/2) END AS mid2       FROM expanded_max_rownum)
+, expanded_with_mid AS (SELECT MAX(rownum) AS max_rownum,
+CASE WHEN MOD(MAX(rownum), 2) = 0 THEN (MAX(rownum)/2) ELSE CEILING(MAX(rownum)/2) END AS mid1,
+CASE WHEN MOD(MAX(rownum), 2) = 0 THEN (MAX(rownum)/2)+1 ELSE CEILING(MAX(rownum)/2) END AS mid2
+FROM expanded_with_rownum)
 SELECT ROUND(AVG(num),1) AS median FROM expanded_with_rownum 
 JOIN expanded_with_mid ON 1 = 1 AND rownum IN (mid1, mid2)
