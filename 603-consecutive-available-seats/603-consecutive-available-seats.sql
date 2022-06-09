7c18a340fe4941b8bc8f2af7c7e79e0c9b2b0d5a
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-06-09 16:59:56 -0400

    Time: 503 ms (18.42%), Space: 0B (100.00%) - LeetHub

--- 603-consecutive-available-seats/603-consecutive-available-seats.sql
+++ 603-consecutive-available-seats/603-consecutive-available-seats.sql
@@ -1,6 +1,12 @@
 # Write your MySQL query statement below
-SELECT c1.seat_id
-FROM Cinema c1
-LEFT OUTER JOIN Cinema c2 ON c2.seat_id = c1.seat_id + 1
-LEFT OUTER JOIN Cinema c3 ON c3.seat_id = c1.seat_id - 1
-WHERE c1.free = 1 AND (c3.free = 1 OR c2.free = 1)
+# SELECT c1.seat_id
+# FROM Cinema c1
+# LEFT OUTER JOIN Cinema c2 ON c2.seat_id = c1.seat_id + 1
+# LEFT OUTER JOIN Cinema c3 ON c3.seat_id = c1.seat_id - 1
+# WHERE c1.free = 1 AND (c3.free = 1 OR c2.free = 1)
+
+SELECT distinct (c1.seat_id)
+FROM Cinema c1 JOIN Cinema c2 ON c1.free = 1 AND c2.free = 1
+AND abs(c1.seat_id-c2.seat_id) = 1 
+ORDER BY c1.seat_id
+
