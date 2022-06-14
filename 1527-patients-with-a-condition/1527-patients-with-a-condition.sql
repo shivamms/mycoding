fb9d1510871cd629c5e4eaf161fc9d975357f8d1
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-06-14 07:32:12 -0400

    Time: 714 ms (5.06%), Space: 0B (100.00%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
+# SELECT patient_id, patient_name, conditions
+# FROM Patients WHERE INSTR(CONCAT(' ',conditions), ' DIAB1') > 0
 SELECT patient_id, patient_name, conditions
-FROM Patients WHERE INSTR(CONCAT(' ',conditions), ' DIAB1') > 0
-# OR INSTR(conditions, ' DIAB1') > 0
+FROM Patients WHERE (conditions LIKE 'DIAB1%'
+OR conditions LIKE '% DIAB1%') AND conditions NOT LIKE '_DIAB1%'
