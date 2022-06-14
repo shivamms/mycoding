9d2d9a5b1ba6b14deb2d66ab2c3821b22d1d7472
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-06-14 10:52:55 -0400

    Time: 942 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 608-tree-node/608-tree-node.sql
+++ 608-tree-node/608-tree-node.sql
@@ -1,11 +1,17 @@
 # # Write your MySQL query statement below
-SELECT id, type FROM
-(SELECT id, 'Root' AS type FROM Tree
-WHERE p_id IS NULL
+# SELECT id, type FROM
+# (SELECT id, 'Root' AS type FROM Tree
+# WHERE p_id IS NULL
+# UNION ALL
+# SELECT id, 'Inner' AS type FROM Tree
+# WHERE p_id IS NOT NULL AND id IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL)
+# UNION ALL
+# SELECT id, 'Leaf' AS type FROM Tree
+# WHERE p_id IS NOT NULL AND id NOT IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL)) a
+# ORDER BY id ASC
+
+SELECT id, type FROM (
+SELECT id, 'Root' AS type FROM Tree WHERE p_id IS NULL
 UNION ALL
-SELECT id, 'Inner' AS type FROM Tree
-WHERE p_id IS NOT NULL AND id IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL)
-UNION ALL
-SELECT id, 'Leaf' AS type FROM Tree
-WHERE p_id IS NOT NULL AND id NOT IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL)) a
-ORDER BY id ASC
+SELECT id, IF (id IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL), 'Inner', 'Leaf') AS type
+FROM Tree WHERE p_id IS NOT NULL) a ORDER BY id
