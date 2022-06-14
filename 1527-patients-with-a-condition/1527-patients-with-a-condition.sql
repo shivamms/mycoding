1c3435212407e311b28ec394b35fc1453707574d
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-06-14 07:32:50 -0400

    Time: 406 ms (28.91%), Space: 0B (100.00%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -2,5 +2,4 @@
 # SELECT patient_id, patient_name, conditions
 # FROM Patients WHERE INSTR(CONCAT(' ',conditions), ' DIAB1') > 0
 SELECT patient_id, patient_name, conditions
-FROM Patients WHERE (conditions LIKE 'DIAB1%'
-OR conditions LIKE '% DIAB1%') AND conditions NOT LIKE '_DIAB1%'
+FROM Patients WHERE conditions LIKE '%DIAB1%' AND conditions NOT LIKE '__DIAB1%'
