7877c7573c9e8e5110092d0305d6a64438f284c0
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-06-14 12:57:29 -0400

    Time: 427 ms (77.92%), Space: 0B (100.00%) - LeetHub

---
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+WITH cte AS 
+(SELECT id, recordDate, temperature, ROW_NUMBER() OVER(ORDER BY recordDate) AS rn FROM Weather)
+SELECT w2.id AS id
+FROM cte w1 JOIN cte w2 ON w2.rn = w1.rn + 1
+WHERE CASE WHEN DATEDIFF(w2.recordDate, w1.recordDate) = 1 
+AND w2.temperature > w1.temperature THEN w2.id END IS NOT NULL
+ORDER BY w1.recordDate
