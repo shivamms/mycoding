ab070291923cf61fd4d4460e0c3479c0605cc186
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-06-14 07:23:10 -0400

    Time: 593 ms (8.26%), Space: 0B (100.00%) - LeetHub

---
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+SELECT patient_id, patient_name, conditions
+FROM Patients WHERE INSTR(CONCAT('*',conditions), '*DIAB1') > 0
+OR INSTR(conditions, ' DIAB1') > 0
