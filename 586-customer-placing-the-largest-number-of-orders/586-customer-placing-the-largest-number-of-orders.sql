1c5862574b7405c07b84145e86dc63d4aee65d6f
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-06-15 15:33:09 -0400

    Time: 1620 ms (5.03%), Space: 0B (100.00%) - LeetHub

--- 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
@@ -1,5 +1,8 @@
 # Write your MySQL query statement below
-WITH ordercount AS 
-(SELECT customer_number, COUNT(order_number) oc FROM Orders
-GROUP BY customer_number)
-SELECT customer_number FROM ordercount WHERE oc = (SELECT MAX(oc) FROM ordercount)
+# WITH ordercount AS 
+# (SELECT customer_number, COUNT(order_number) oc FROM Orders
+# GROUP BY customer_number)
+# SELECT customer_number FROM ordercount WHERE oc = (SELECT MAX(oc) FROM ordercount)
+
+# For followup question
+SELECT customer_number FROM Orders GROUP BY customer_number ORDER BY COUNT(1) DESC LIMIT 1
