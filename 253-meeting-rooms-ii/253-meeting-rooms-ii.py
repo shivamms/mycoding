4ac650db568d53ccd1615f8efeaeb7a2a6ac6dd5
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-07-02 07:03:25 -0400

    Time: 164 ms (12.00%), Space: 18 MB (9.20%) - LeetHub

---
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[0])
+        end_time = []
+        heapq.heappush(end_time, intervals[0][1])
+        for m in intervals[1:]:
+            if m[0] < end_time[0]:
+                heapq.heappush(end_time, m[1])
+            else:
+                heapq.heapreplace(end_time, m[1])
+            print(end_time)
+        print(intervals)
+        
+        return(len(end_time))
+            
