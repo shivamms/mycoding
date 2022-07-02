a94d3422433891a0fa72474c2fc0b9dd6d4e38c9
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-07-02 07:46:24 -0400

    Time: 32 ms (91.57%), Space: 13.9 MB (73.73%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.py
+++ 844-backspace-string-compare/844-backspace-string-compare.py
@@ -1,21 +1,32 @@
 class Solution:
-    def backspaceCompare(self, s: str, t: str) -> bool:
-        x,y='',''
-        bs_count = 0
-        for c in s[::-1]:
-            if c == '#':
-                bs_count += 1
-            elif bs_count > 0:
-                bs_count -= 1
-            else:
-                x += c
-        bs_count = 0
-        for c in t[::-1]:
-            if c == '#':
-                bs_count += 1
-            elif bs_count > 0:
-                bs_count -= 1
-            else:
-                y += c
-        return x == y
-            
+    # def backspaceCompare(self, s: str, t: str) -> bool:
+    #     x,y='',''
+    #     bs_count = 0
+    #     for c in s[::-1]:
+    #         if c == '#':
+    #             bs_count += 1
+    #         elif bs_count > 0:
+    #             bs_count -= 1
+    #         else:
+    #             x += c
+    #     bs_count = 0
+    #     for c in t[::-1]:
+    #         if c == '#':
+    #             bs_count += 1
+    #         elif bs_count > 0:
+    #             bs_count -= 1
+    #         else:
+    #             y += c
+    #     return x == y
+    def backspaceCompare(self, S, T):
+        def F(S):
+            skip = 0
+            for x in reversed(S):
+                if x == '#':
+                    skip += 1
+                elif skip:
+                    skip -= 1
+                else:
+                    yield x
+
+        return all(x == y for x, y in itertools.zip_longest(F(S), F(T)))
