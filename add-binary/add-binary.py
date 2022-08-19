889749aa5b56270fa8d47f1ca166d06ca1fc74b1
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-08-19 07:55:55 -0400

    Time: 72 ms, Memory: 13.9 MB - LeetHub

--- add-binary/add-binary.py
+++ add-binary/add-binary.py
@@ -1,9 +1,10 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        if len(a) == 1 and len(b) == 1 and a[0] == "0" and b [0] == "0":
+        m,n = len(a), len(b)
+        if m == 1 and n == 1 and a[0] == "0" and b [0] == "0":
             return "0"
         result, dec1, dec2 = "", 0,0
-        i,j,m,n = 0,0,len(a), len(b)
+        i,j = 0,0
         a,b = a[::-1], b[::-1]
         while i < m:
             dec1 += (ord(a[i])-48) * (2 ** i)
