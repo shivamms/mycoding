4a420b688e22270d760d3b5345be264a56711a4e
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-08-19 01:02:24 -0400

    Time: 192 ms, Memory: 19.1 MB - LeetHub

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -1,7 +1,18 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         from collections import defaultdict
-        hashmap = defaultdict(list)
+        # using sorting
+        # hashmap = defaultdict(list)
+        # for s in strs:
+        #     hashmap[''.join(sorted(s))].append(s)
+        # return [val for key,val in hashmap.items()]
+    
+        # using direct access array 
+        hashmap  = defaultdict(list)
         for s in strs:
-            hashmap[''.join(sorted(s))].append(s)
+            count = [0] * 26
+            for c in s:
+                count[ord(c)-ord('a')] += 1
+            hashmap[tuple(count)].append(s)
         return [val for key,val in hashmap.items()]
+        
