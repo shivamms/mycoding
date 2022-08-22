25d58456cb120099426902cd0e4074a879ac2cd3
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-08-22 14:10:38 -0400

    Time: 60 ms (31.11%), Space: 13.9 MB (88.29%) - LeetHub

--- 161-one-edit-distance/161-one-edit-distance.py
+++ 161-one-edit-distance/161-one-edit-distance.py
@@ -1,30 +1,40 @@
 class Solution:
     def isOneEditDistance(self, s: str, t: str) -> bool:
-        m,n = len(s), len(t)
-        if abs(m-n) > 1 or (not m and not n):
-            return False
-        i,j = 0, 0
-        diffSeen = 0
-        while i < m and j < n:
-            if s[i] != t[j]:
-                if diffSeen > 0:
-                    return False
-                else:
-                    diffSeen += 1
-                    if m > n:
-                        i += 1
-                    elif m < n:
-                        j += 1
-                    else:
-                        i += 1
-                        j += 1
-            else:
-                i += 1
-                j += 1
-        if diffSeen == 0 and (m-i == 1 or n-j == 1):
-            diffSeen += 1
-        return True if diffSeen == 1 else False
+        # m,n = len(s), len(t)
+        # if abs(m-n) > 1 or (not m and not n):
+        #     return False
+        # i,j = 0, 0
+        # diffSeen = 0
+        # while i < m and j < n:
+        #     if s[i] != t[j]:
+        #         if diffSeen > 0:
+        #             return False
+        #         else:
+        #             diffSeen += 1
+        #             if m > n:
+        #                 i += 1
+        #             elif m < n:
+        #                 j += 1
+        #             else:
+        #                 i += 1
+        #                 j += 1
+        #     else:
+        #         i += 1
+        #         j += 1
+        # if diffSeen == 0 and (m-i == 1 or n-j == 1):
+        #     diffSeen += 1
+        # return True if diffSeen == 1 else False
         
+        m,n = len(s), len(t)
+        if m > n: return self.isOneEditDistance(t, s)
+        if n-m > 1: return False
+        for i in range(m):
+            if s[i] != t[i]:
+                if m == n:
+                    return s[i+1:] == t[i+1:]
+                else:
+                    return s[i:] == t[i+1:]
+        return m+1 == n
         
                         
         
