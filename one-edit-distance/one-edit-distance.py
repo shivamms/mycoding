d87672d3ac42cdc5284382ef4bdf4d3ff4caee02
Subramanian Mayakkaruppan <shivam.ms@gmail.com>
2022-08-22 13:27:39 -0400

    Time: 120 ms, Memory: 14.2 MB - LeetHub

---
+++ one-edit-distance/one-edit-distance.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isOneEditDistance(self, s: str, t: str) -> bool:
+        m,n = len(s), len(t)
+        if abs(m-n) > 1 or (not m and not n):
+            return False
+        i,j = 0, 0
+        diffSeen = 0
+        while i < m and j < n:
+            print(i,j)
+            if s[i] != t[j]:
+                if diffSeen > 0:
+                    return False
+                else:
+                    diffSeen += 1
+                    if m > n:
+                        i += 1
+                    elif m < n:
+                        j += 1
+                    else:
+                        i += 1
+                        j += 1
+            else:
+                i += 1
+                j += 1
+        if diffSeen == 0 and (m-i == 1 or n-j == 1):
+            diffSeen += 1
+        return True if diffSeen == 1 else False
+                        
+        
+        
